434a2ba00e26ab36b2e68ba88fbc40744b2fba3b
pixeland10 <pro.pixeland@gmail.com>
2026-08-27 00:49:27 +0200

    Add files via upload

---
+++ config.js
@@ -0,0 +1,5 @@
+window.PL_CONFIG = {
+  SB_URL: "https://ggncarjlqchezdbxnfsg.supabase.co",
+  SB_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdnbmNhcmpscWNoZXpkYnhuZnNnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODc2ODg4MzcsImV4cCI6MjEwMzI2NDgzN30.TQCPAw_Ov5hooPdH2EqZx9YHWqoiro91JXB8wltm71o",
+  ADMIN_SECRET: "maisonjulai2024"
+};
